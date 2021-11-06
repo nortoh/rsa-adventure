11005b64aecbc24f6c3b81b979a6cccc63329c07
Christian Horton <christian@christianhorton.me>
2021-11-06 23:16:02 +0000

    Added gcd, e generation, and rand_num generation

--- rsa.c
+++ rsa.c
@@ -26,24 +26,50 @@ int get_prime() {
     int prime;
 
     do {
-        int n = rand() % 100;
+        int n = rand() % 10;
         prime = pow(n, 2) + n + 41; 
     } while (!is_prime(prime));
     
     printf("Generarting prime: %d\n", prime);
+
     return prime;
 }
 
 int pq_key(int *p, int *q) {
     *p = get_prime();
     *q = get_prime();
-    return *p * *q;
+    return abs(*p * *q);
 }
 
+int gcd(int a, int b) {
+    while(a!=b) {
+        if(a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    return a;
+}
+
+int e_key(int p, int q) {
+    int r;
+    int phi_pq = (p - 1) * (q - 1);
+    int e;
+    int g = 0;
+
+    while(g != 1) {
+        r = rand() % 15;
+        e = pow(2, r) + 1;
+        g = gcd(e, phi_pq);
+        printf("gcd=%d\n", g);
+    }
+    
+    return e;
+}
 
 int main(int argc, char **argv) {
     printf("Starting custom RSA implementation\n");
-    srand(time(NULL));
+    srand(time(0));
 
     if(is_prime(3)) {
         printf("This number is prime\n");
@@ -53,9 +79,10 @@ int main(int argc, char **argv) {
 
     int p;
     int q;
-    int result = pq_key(&p, &q);
+    int n = pq_key(&p, &q);
 
-    printf("p=%d, q=%d, n=%d\n");
+    int e = e_key(p, q);
+    printf("p=%d, q=%d, n=%d, e=%d\n", p, q, n, e);
 
 
     char *message = "This is my secret message";
